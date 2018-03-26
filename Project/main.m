93218bbfd473cec8252959b871c209654b01f8e0
Mike Burke <mikeburke@Mikes-MacBook-Air.local>
2018-03-26 19:21:49 -0400

    Initial Commit

---
+++ Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project
+//
+//  Created by Mike Burke on 3/26/18.
+//  Copyright © 2018 Mike Burke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
